1032e2f1d8795e342f53a3d45e34caebaaab7609
cygan3107 <krzysztof.malon@onet.pl>
2024-04-03 12:56:46 +0200

    Summary

---
+++ task-2.js
@@ -0,0 +1,7 @@
+"use strict";
+function getShippingMessage(country, price, deliveryFee) {
+    console.log(`Shipping ${country} will cost ${price * deliveryFee} credits`);
+}
+getShippingMessage("Australia", 120, 50); // "Shipping to Australia will cost 170 credits"
+getShippingMessage("Germany", 80, 20); // "Shipping to Germany will cost 100 credits"
+getShippingMessage("Sweden", 100, 20); // "Shipping to Sweden will cost 120 credits"
